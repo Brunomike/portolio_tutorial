73123dfbd5b5d364e7962912c0150bcd182fe507
Michael Bruno <brunomike254@gmail.com>
2022-06-03 22:09:42 +0300

    Started working on the Work container

--- frontend-react/src/container/Work/Work.jsx
+++ frontend-react/src/container/Work/Work.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrapper } from '../../hoc';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({y: 0,opacity: 1});
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query='*[_type == "works"]';
+
+    client.fetch(query)
+    .then((data) =>{
+      setWorks(data);
+      setFilterWork(data);
+    })
+  }, [])
+
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative<span> Portfolio</span></h2>
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`} >
+            {item}
+          </div>
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+
+      </motion.div>
+    </>
   )
 }
 
-export default Work;
+export default AppWrapper(Work, 'work');
